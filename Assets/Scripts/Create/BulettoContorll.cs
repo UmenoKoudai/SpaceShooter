e0cf9f2bf1fcdca8d3b7040a6171f15edd240121
umenokoudai <umenokoudai0805@gmail.com>
2022-06-08 11:23:21 +0900

    chargゲージ要素追加

--- Assets/Scripts/Create/BulettoContorll.cs
+++ Assets/Scripts/Create/BulettoContorll.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class BulettoContorll : MonoBehaviour
 {
     [SerializeField] float m_position = 8.0f;
+    [SerializeField] trumpController _charge;
+    int _count;
     // Start is called before the first frame update
     void Start()
     {
-        
+        trumpController _charge = GetComponent<trumpController>();
     }
 
     // Update is called once per frame
@@ -17,6 +19,14 @@ public class BulettoContorll : MonoBehaviour
         if(transform.position.y > m_position)
         {
             Destroy(gameObject);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            _count++;
+            _charge._count = _count;
         }
     }
 }
--- Assets/Scripts/trumpController.cs
+++ Assets/Scripts/trumpController.cs
@@ -5,16 +5,16 @@ using UnityEngine.UI;
 
 public class trumpController : MonoBehaviour
 {
-    [SerializeField] Image _Gauge1;
-    [SerializeField] Image _Gauge2;
-    [SerializeField] Image _Gauge3;
-    [SerializeField] Image _Gauge4;
-    [SerializeField] Image _Gauge5;
-    [SerializeField] Image _Gauge6;
-    [SerializeField] Image _Gauge7;
-    [SerializeField] Image _Gauge8;
-    [SerializeField] Image _Gauge9;
-    [SerializeField] Image _GaugeFull;
+    [SerializeField] Image _Gauge1 = default;
+    [SerializeField] Image _Gauge2 = default;
+    [SerializeField] Image _Gauge3 = default;
+    [SerializeField] Image _Gauge4 = default;
+    [SerializeField] Image _Gauge5 = default;
+    [SerializeField] Image _Gauge6 = default;
+    [SerializeField] Image _Gauge7 = default;
+    [SerializeField] Image _Gauge8 = default;
+    [SerializeField] Image _Gauge9 = default;
+    [SerializeField] Image _GaugeFull = default;
     ParticleSystem _particle;
     int _count = 0;
     // Start is called before the first frame update
@@ -28,56 +28,52 @@ public class trumpController : MonoBehaviour
     {
         
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    void Charge(int _count)
     {
-        if(collision.gameObject.tag == "Enemy")
+        switch (_count)
         {
-            
-            _count++;
-            switch(_count)
-            {
-                case 1:
-                    _Gauge1.gameObject.SetActive(false);
-                    _Gauge2.gameObject.SetActive(true);
-                    break;
-                case 2:
-                    _Gauge1.gameObject.SetActive(false);
-                    _Gauge3.gameObject.SetActive(true);
-                    break;
-                case 3:
-                    _Gauge1.gameObject.SetActive(false);
-                    _Gauge4.gameObject.SetActive(true);
-                    break;
-                case 4:
-                    _Gauge1.gameObject.SetActive(false);
-                    _Gauge5.gameObject.SetActive(true);
-                    break;
-                case 5:
-                    _Gauge1.gameObject.SetActive(false);
-                    _Gauge6.gameObject.SetActive(true);
-                    break;
-                case 6:
-                    _Gauge1.gameObject.SetActive(false);
-                    _Gauge7.gameObject.SetActive(true);
-                    break;
-                case 7:
-                    _Gauge1.gameObject.SetActive(false);
-                    _Gauge8.gameObject.SetActive(true);
-                    break;
-                case 8:
-                    _Gauge1.gameObject.SetActive(false);
-                    _Gauge9.gameObject.SetActive(true);
-                    break;
-                default:
-                    _Gauge1.gameObject.SetActive(false);
-                    _GaugeFull.gameObject.SetActive(true);
-                    if(Input.GetButtonDown("Enter"))
-                    {
-                        _particle.Play();
-                        _count = 0;
-                    }
-                    break;
-            }
+            case 1:
+                _Gauge1.gameObject.SetActive(false);
+                _Gauge2.gameObject.SetActive(true);
+                break;
+            case 2:
+                _Gauge1.gameObject.SetActive(false);
+                _Gauge3.gameObject.SetActive(true);
+                break;
+            case 3:
+                _Gauge1.gameObject.SetActive(false);
+                _Gauge4.gameObject.SetActive(true);
+                break;
+            case 4:
+                _Gauge1.gameObject.SetActive(false);
+                _Gauge5.gameObject.SetActive(true);
+                break;
+            case 5:
+                _Gauge1.gameObject.SetActive(false);
+                _Gauge6.gameObject.SetActive(true);
+                break;
+            case 6:
+                _Gauge1.gameObject.SetActive(false);
+                _Gauge7.gameObject.SetActive(true);
+                break;
+            case 7:
+                _Gauge1.gameObject.SetActive(false);
+                _Gauge8.gameObject.SetActive(true);
+                break;
+            case 8:
+                _Gauge1.gameObject.SetActive(false);
+                _Gauge9.gameObject.SetActive(true);
+                break;
+            default:
+                _Gauge1.gameObject.SetActive(false);
+                _GaugeFull.gameObject.SetActive(true);
+                if (Input.GetButtonDown("Enter"))
+                {
+                    _particle.Play();
+                    _count = 0;
+                }
+                break;
         }
     }
 }
