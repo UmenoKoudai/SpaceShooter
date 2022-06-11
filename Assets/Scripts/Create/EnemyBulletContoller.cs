af700964b2d353971b976d452977703c5f947a62
umenokoudai <umenokoudai0805@gmail.com>
2022-06-11 22:24:08 +0900

    BossEnemy修正(弾の方向)

--- Assets/Scripts/Create/EnemyBulletContoller.cs
+++ Assets/Scripts/Create/EnemyBulletContoller.cs
@@ -6,12 +6,15 @@ using UnityEngine.UI;
 public class EnemyBulletContoller : MonoBehaviour
 {
     [SerializeField] float m_position = -5f;
+    [SerializeField] float m_initialSpeed = 3f;
     GameObject m_damage;
     //[SerializeField] GameObject m_damage;
     //Start is called before the first frame update
     void Start()
     {
         m_damage = GameObject.Find("LifeController");
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.velocity = this.transform.up * m_initialSpeed;
     }
 
     // Update is called once per frame
